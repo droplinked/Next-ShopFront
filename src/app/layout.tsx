6b7dd444ac1b7dc0396a7319c791a938ebcf6588
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2024-02-13 11:39:02 +0330

    Fix the issue with theme transitions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
+import { AppThemeProvider } from "@/components/providers/theme/AppThemeProvider";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { AppThemeProvider } from "@/components/providers/theme/AppThemeProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,7 +14,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
     return (
         <html lang="en" suppressHydrationWarning>
             <body className={inter.className}>
-                <AppThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange storageKey="droplinked-theme">
+                <AppThemeProvider>
                     {children}
                 </AppThemeProvider>
             </body>
--- src/components/providers/theme/AppThemeProvider.tsx
+++ src/components/providers/theme/AppThemeProvider.tsx
@@ -1,9 +1,18 @@
 "use client";
 
-import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
 
-export function AppThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+export function AppThemeProvider({ children }: ThemeProviderProps) {
+  return <NextThemesProvider
+    attribute="class"
+    defaultTheme="system"
+    enableSystem={true}
+    enableColorScheme={true}
+    themes={['light', 'dark']}
+    disableTransitionOnChange
+    storageKey="droplinked-theme"
+  >
+    {children}
+  </NextThemesProvider>
 }
