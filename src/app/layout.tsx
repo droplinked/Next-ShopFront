0598e124d8c29c611bdb742258a48a5e96feeab7
MajidHosseinzadeh <MajidHosseinzadehh@gmail.com>
2024-02-14 17:06:05 +0330

    layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { AppThemeProvider } from "@/components/providers/theme/AppThemeProvider"
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/layout/header/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,6 +15,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
     return (
         <html lang="en" suppressHydrationWarning>
             <body className={inter.className}>
+                <Header/>
                 <AppThemeProvider>
                     {children}
                 </AppThemeProvider>
