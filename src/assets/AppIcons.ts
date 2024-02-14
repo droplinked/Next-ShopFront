a40221ba1fc6ec0522558c34acd3c40fae827186
MajidHosseinzadeh <MajidHosseinzadehh@gmail.com>
2024-02-14 17:05:21 +0330

    icons and its component

---
+++ src/assets/AppIcons.ts
@@ -0,0 +1,13 @@
+import Search from "./icons/search.svg";
+import Slash from "./icons/slash.svg";
+import User from "./icons/user.svg";
+import Cart from "./icons/cart.svg";
+
+const AppIcons = {
+    Search,
+    Slash,
+    User,
+    Cart,
+};
+
+export default AppIcons;
--- src/assets/app-icons.ts
+++
@@ -1,7 +0,0 @@
-// import svgicon from './icons/icon.svg'
-
-const AppIcons = {
-
-}
-
-export default AppIcons
