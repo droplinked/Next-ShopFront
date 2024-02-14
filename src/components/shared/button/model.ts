ddaf1a3d04bc98739a6dd8765c7189a2fecf7607
MajidHosseinzadeh <MajidHosseinzadehh@gmail.com>
2024-02-14 17:06:20 +0330

    updated button style

--- src/components/shared/button/model.ts
+++ src/components/shared/button/model.ts
@@ -1,7 +1,7 @@
 import { cva } from "class-variance-authority";
 
 namespace appButtonModel {
-    export const variants = cva("px-4 disabled:pointer-events-none disabled:bg-secondary", {
+    export const variants = cva("px-4 inline-flex items-center justify-center disabled:pointer-events-none disabled:bg-secondary", {
         variants: {
             appVariant: {
                 filled: "border-none bg-foreground ",
