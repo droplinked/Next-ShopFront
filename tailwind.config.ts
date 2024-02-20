bea9856497a39ffce4714a735bc5ed419ce34d78
MajidHosseinzadeh <MajidHosseinzadehh@gmail.com>
2024-02-20 21:52:37 +0330

    css variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,8 @@ const config: Config = {
     theme: {
         container: {
             center: true,
-            padding: "2rem",
+            padding: "72px",
+            
             screens: {
                 "2xl": "1400px",
             },
