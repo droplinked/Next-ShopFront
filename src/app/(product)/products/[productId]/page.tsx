8272cf107e813386a549b935cb81b58687f37ef8
MajidHosseinzadeh <MajidHosseinzadehh@gmail.com>
2024-02-20 21:53:23 +0330

    product page

--- src/app/(product)/products/[productId]/page.tsx
+++ src/app/(product)/products/[productId]/page.tsx
@@ -1,7 +1,19 @@
-const ProductPage = () => {
-  return (
-    <div>ProductPage</div>
-  )
-}
+import { fetchInstance } from "@/lib/apis/fetch-config";
+import ProductDetails from "./parts/details/product-details";
+import ProductSlider from "./parts/slider/product-slider";
 
-export default ProductPage
+const ProductPage = async ({ params }: { params: { productId: string } }) => {
+    const data = await fetchInstance(`products/${params.productId}`);
+    return (
+        <main className="md:container pt-20 px-8 flex items-start md:flex-row flex-col justify-center w-full gap-12">
+                <div className="min-w-full md:min-w-[40%]">
+                    <ProductSlider media={data?.media} />
+                </div>
+                <div className="min-w-full md:min-w-[60%]">
+                    <ProductDetails product={data} />
+                </div>
+        </main>
+    );
+};
+
+export default ProductPage;
