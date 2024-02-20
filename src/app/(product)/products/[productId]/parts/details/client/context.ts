3f3ff08b4d4c7ea527723d5ebaefe3274d9dc64a
MajidHosseinzadeh <MajidHosseinzadehh@gmail.com>
2024-02-20 21:55:41 +0330

    product context

---
+++ src/app/(product)/products/[productId]/parts/details/client/context.ts
@@ -0,0 +1,27 @@
+import initialProductProps, { IProduct, ISku, initialSkuProps } from "@/app/types/interfaces/product/product";
+import { createContext } from "react";
+
+export interface IProductClientState {
+    product: IProduct;
+    option: { color: string | null; size: string | null; quantity: number };
+    sku: ISku;
+    ruleset: { loading: boolean; data: any };
+}
+export const initialProductClientState: IProductClientState = {
+    product: initialProductProps,
+    option: { color: null, size: null, quantity: 1 },
+    sku: initialSkuProps,
+    ruleset: { loading: false, data: false },
+};
+
+interface IProductContext {
+    states: IProductClientState;
+    methods: { updateState: Function; updateOption: Function };
+}
+
+const ProductContext = createContext<IProductContext>({
+    states: initialProductClientState,
+    methods: { updateOption: () => {}, updateState: () => {} },
+});
+
+export default ProductContext;
