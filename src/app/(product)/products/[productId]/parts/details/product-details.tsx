92d1f8f75c184a297fb2b5ae012e6a7780e2f3ea
MajidHosseinzadeh <MajidHosseinzadehh@gmail.com>
2024-02-20 21:57:11 +0330

    product details

---
+++ src/app/(product)/products/[productId]/parts/details/product-details.tsx
@@ -0,0 +1,14 @@
+import { roboto } from "@/app/styles/fonts";
+import { cn } from "@/lib/utils/cn/cn";
+import ProductClient from "./client/product-client";
+
+const ProductDetails = ({ product }: { product: any }) => {
+    return (
+        <section className="w-full">
+            <h1 className={cn("text-3xl font-medium", roboto.className)}>{product.title}</h1>
+            <ProductClient product={product} />
+        </section>
+    );
+};
+
+export default ProductDetails;
