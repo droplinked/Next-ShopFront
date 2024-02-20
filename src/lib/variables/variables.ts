5c9a228c3b999115d527035ae659d5132e668b22
MajidHosseinzadeh <MajidHosseinzadehh@gmail.com>
2024-02-20 21:54:42 +0330

    app variables

--- src/lib/variables/variables.ts
+++ src/lib/variables/variables.ts
@@ -1,1 +1,8 @@
-export const BASE_API_URL = process.env.NEXT_PUBLIC_BASE_API_URL
+export const BASE_API_URL = process.env.NEXT_PUBLIC_BASE_API_URL;
+
+export const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+
+export const variantIDs = {
+    color: { _id: "62a989ab1f2c2bbc5b1e7153" },
+    size: { _id: "62a989e21f2c2bbc5b1e7154" },
+};
