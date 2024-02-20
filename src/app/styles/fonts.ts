63414c4dce83c89c0c9a6d1f9dec7efa5a916c46
MajidHosseinzadeh <MajidHosseinzadehh@gmail.com>
2024-02-20 21:54:17 +0330

    fonts

--- src/app/styles/fonts.ts
+++ src/app/styles/fonts.ts
@@ -1,4 +1,9 @@
-import localFont from 'next/font/local'
-const Avenir = localFont({ src: './AvenirNext.otf' })
- 
-export { Avenir }
+import localFont from "next/font/local";
+import { Roboto } from "next/font/google";
+const Avenir = localFont({ src: "./AvenirNext.otf" });
+
+const roboto = Roboto({
+    weight: ["500", "700", "900"],
+    subsets: ['latin']
+});
+export { Avenir, roboto };
