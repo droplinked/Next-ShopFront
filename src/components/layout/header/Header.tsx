4009802918d120384906bd3bc5a3cf153f859720
MajidHosseinzadeh <MajidHosseinzadehh@gmail.com>
2024-02-20 21:54:59 +0330

    header layout

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -9,7 +9,7 @@ import React from "react";
 
 const Header: React.FC = () => {
     return (
-        <header className="sticky top-0 w-full bg-white py-4 shadow-[0_4px_10px_0px_rgba(0,0,0,0.05)]">
+        <header className="sticky top-0 z-20 w-full bg-white py-4 shadow-[0_4px_10px_0px_rgba(0,0,0,0.05)]">
             <div className="container mx-auto flex justify-between items-center">
                 <div className="flex items-center space-x-4">
                     <Image src={droplinked} alt="Site Logo" className="h-12 object-contain" width={100} height={32} />
