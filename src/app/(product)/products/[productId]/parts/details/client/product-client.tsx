af9672e180931a3c291f7a4687e1c8f6ec9475e0
MajidHosseinzadeh <MajidHosseinzadehh@gmail.com>
2024-02-22 14:26:36 +0330

    product client

--- src/app/(product)/products/[productId]/parts/details/client/product-client.tsx
+++ src/app/(product)/products/[productId]/parts/details/client/product-client.tsx
@@ -5,9 +5,11 @@ import productClientModel from "./model";
 import { IProduct } from "@/app/types/interfaces/product/product";
 import ProductPrice from "./parts/price/product-price";
 import ProductColors from "./parts/variants/colors/product-colors";
+import ProductSizes from "./parts/variants/sizes/product-sizes";
+import ProductQuantity from "./parts/quantity/product-quantity";
+import AddToCart from "./parts/add/add-to-cart";
 
 const ProductClient = ({ product }: { product: IProduct }) => {
-    // const checkeRuleService = useMutation((params: IcheckRulesetService) => checkRulesetService(params));
     const [States, setStates] = useState<IProductClientState>(initialProductClientState);
     const { getFirstOption, findSkuAsOption } = productClientModel;
     const updateState = (key: string, value: any) => setStates((prev: IProductClientState) => ({ ...prev, [key]: value }));
@@ -18,8 +20,13 @@ const ProductClient = ({ product }: { product: IProduct }) => {
 
     return (
         <ProductContext.Provider value={{ states: States, methods: { updateOption, updateState } }}>
-            <ProductPrice />
-            <ProductColors/>
+            <div className="flex flex-col gap-9">
+                <ProductPrice />
+                <ProductColors/>
+                <ProductSizes/>
+                <ProductQuantity/>
+                <AddToCart/>
+            </div>
         </ProductContext.Provider>
     );
 };
