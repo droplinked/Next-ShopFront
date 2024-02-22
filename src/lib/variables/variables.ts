a71a8487a71c5dcf8f4f918c2774a4fa068a88b5
MajidHosseinzadeh <MajidHosseinzadehh@gmail.com>
2024-02-22 14:21:33 +0330

    update variables

--- src/lib/variables/variables.ts
+++ src/lib/variables/variables.ts
@@ -1,8 +1,4 @@
 export const BASE_API_URL = process.env.NEXT_PUBLIC_BASE_API_URL;
-
 export const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
-
-export const variantIDs = {
-    color: { _id: "62a989ab1f2c2bbc5b1e7153" },
-    size: { _id: "62a989e21f2c2bbc5b1e7154" },
-};
+export const APP_DEVELOPMENT = process.env.NEXT_PUBLIC_APP_DEVELOPMENT === "true";
+export const variantIDs = { color: { _id: "62a989ab1f2c2bbc5b1e7153" }, size: { _id: "62a989e21f2c2bbc5b1e7154" }};
