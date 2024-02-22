9f9daae4b55fc63828c49eeb459329eadfe27838
MajidHosseinzadeh <MajidHosseinzadehh@gmail.com>
2024-02-22 14:19:50 +0330

    icons

--- src/assets/AppIcons.ts
+++ src/assets/AppIcons.ts
@@ -2,12 +2,20 @@ import Search from "./icons/search.svg";
 import Slash from "./icons/slash.svg";
 import User from "./icons/user.svg";
 import Cart from "./icons/cart.svg";
+import CartLight from "./icons/cartlight.svg";
+import Min from "./icons/min.svg";
+import Plus from "./icons/plus.svg";
+import Merch from "./icons/merch.svg";
 
 const AppIcons = {
+    CartLight,
     Search,
     Slash,
+    Merch,
     User,
+    Plus,
     Cart,
+    Min,
 };
 
 export default AppIcons;
