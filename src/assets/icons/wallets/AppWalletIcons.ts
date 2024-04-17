d3889757992580a4ece871363d487b7097d94985
MajidHosseinzadeh <MajidHosseinzadehh@gmail.com>
2024-04-17 12:50:25 +0330

    feat: wallet icons

--- src/assets/icons/wallets/AppWalletIcons.ts
+++ src/assets/icons/wallets/AppWalletIcons.ts
@@ -1,8 +1,17 @@
 import CasperPayment from "./payment/casper-payment.svg";
 import UnstoppablePayment from "./payment/unstoppable-payment.svg";
 import StripePayment from "./payment/stripe-payment.svg";
+import BasePayment from "./base-logo-in-blue.svg";
+import BinancePayment from "./binance.svg";
+import ETHPayment from "./eth.svg";
+import HederaPayment from "./hedera.svg";
+import LinaePayment from "./linea.svg";
+import NearPayment from "./near-.svg";
+import PolygonPayment from "./polygon.svg";
+import BitcoinPayment from "./bitcoin.svg";
+import XrplSidechainPayment from "./xrpl-sidechain-.svg";
 
-const AppWalletIcons = { CasperPayment, UnstoppablePayment, StripePayment };
+const AppWalletIcons = { CasperPayment, UnstoppablePayment, StripePayment, BasePayment, BinancePayment, ETHPayment, HederaPayment, LinaePayment, NearPayment, PolygonPayment, BitcoinPayment, XrplSidechainPayment};
 export default AppWalletIcons;
 
 
